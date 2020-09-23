4e72f0199f8bb84af5a4d3e052490b1e59dbc1b5
PERSCENT\jakub.marciniszyn <jakub.marciniszyn@tfs.com>
2020-09-23 13:23:48 +0100

    hashtag generator

---
+++ 2020-09-23/directions-reductions.ts
@@ -0,0 +1,15 @@
+function dirReduc(arr) {
+    const counts = {};
+
+    for (let i = 0; i < arr.length; i++) {
+        const direction = arr[i].toLowerCase();
+        counts[direction] = counts[direction] || 0;
+        counts[direction]++;
+    }
+
+    console.log(counts);
+}
+
+console.log(dirReduc(['NORTH', 'SOUTH', 'SOUTH', 'EAST', 'WEST', 'NORTH', 'WEST']), ['WEST']);
+console.log(dirReduc(['NORTH', 'WEST', 'SOUTH', 'EAST']), ['NORTH', 'WEST', 'SOUTH', 'EAST']);
+console.log(dirReduc(['NORTH', 'SOUTH', 'EAST', 'WEST', 'EAST', 'WEST']), []);
---
+++ 2020-09-23/the-hashtag-generaor.ts
@@ -0,0 +1,15 @@
+function generateHashtag(str) {
+    if (!str) return false;
+    const clean = str.replace(/(\s*)\b(\w{1})/g, (_, p1, p2) => p2.toUpperCase());
+    return clean.length >= 140 ? false : `#${clean}`;
+}
+
+//console.log(generateHashtag(''), false);
+//console.log(generateHashtag(' '.repeat(200)), false);
+console.log(generateHashtag('Do We have A Hashtag'), '#DoWeHaveAHashtag');
+console.log(generateHashtag('Codewars'), '#Codewars');
+console.log(generateHashtag('Codewars Is Nice'), '#CodewarsIsNice');
+console.log(generateHashtag('Codewars is nice'), '#CodewarsIsNice');
+console.log(generateHashtag('code' + ' '.repeat(140) + 'wars'), '#CodeWars');
+console.log(generateHashtag('a'.repeat(139)), '#A' + 'a'.repeat(138));
+console.log(generateHashtag('a'.repeat(140)), false);
