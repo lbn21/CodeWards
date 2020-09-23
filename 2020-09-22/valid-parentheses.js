e1a990828ca90fdf832410d6994bbf9a32a633cf
PERSCENT\jakub.marciniszyn <jakub.marciniszyn@tfs.com>
2020-09-23 12:44:34 +0100

    solved with while loop

--- 2020-09-22/valid-parentheses.js
+++ 2020-09-22/valid-parentheses.js
@@ -1,6 +1,10 @@
 function validParentheses(parens) {
-    return Array.prototype.reduce.call(parens, (str, _) => str.replace(/\(\)/g, ''), parens).length === 0;
+    while (parens.match(/\(\)/g)) {
+        parens = parens.replace(/\(\)/g, '');
+    }
+    return parens.length === 0;
 }
 console.log(validParentheses('(())(())()()()(())()(())()()(()('), false);
 console.log(validParentheses('()()()(('), false);
 console.log(validParentheses('())'), false);
+console.log(validParentheses('()(())()()()(())()()()()()()((()))()()'), true);
