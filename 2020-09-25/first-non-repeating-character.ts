c8c46f32044d5a6cd8fa747f602013c75244dc86
PERSCENT\jakub.marciniszyn <jakub.marciniszyn@tfs.com>
2020-09-25 14:53:37 +0100

    non repeating characters

---
+++ 2020-09-25/first-non-repeating-character.ts
@@ -0,0 +1,23 @@
+function firstNonRepeatingLetter(s) {
+    const seen = {};
+    for (let i = 0; i < s.length; i++) {
+        const letter = s[i].toLowerCase();
+        seen[letter] = seen[letter] || { count: 0, first: s[i] };
+        seen[letter]['count']++;
+    }
+
+    for (const letter in seen) {
+        const info = seen[letter];
+        if (info['count'] === 1) {
+            return info['first'];
+        }
+    }
+
+    return '';
+}
+
+console.log(firstNonRepeatingLetter('a'), 'a');
+console.log(firstNonRepeatingLetter('stress'), 't');
+console.log(firstNonRepeatingLetter('moonmen'), 'e');
+console.log(firstNonRepeatingLetter('sTreSS'), 'T');
+console.log(firstNonRepeatingLetter('abba'), '');
