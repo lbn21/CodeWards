0bd0dbbbc29467f0448c897dec3571247c16c6cf
PERSCENT\jakub.marciniszyn <jakub.marciniszyn@tfs.com>
2020-09-28 10:50:21 +0100

    directions reductions

--- 2020-09-23/directions-reductions.js
+++ 2020-09-23/directions-reductions.js
@@ -1,12 +1,45 @@
 function dirReduc(arr) {
-    const counts = {};
-    for (let i = 0; i < arr.length; i++) {
-        const direction = arr[i].toLowerCase();
-        counts[direction] = counts[direction] || 0;
-        counts[direction]++;
+    if (!arr.length)
+        return arr;
+    const counterDirections = {
+        north: 'south',
+        south: 'north',
+        east: 'west',
+        west: 'east',
+    };
+    const directions = [];
+    directions.push(arr[0]);
+    for (let i = 1; i < arr.length; i++) {
+        const dir = arr[i];
+        if (directions.length &&
+            directions[directions.length - 1].toLowerCase() === counterDirections[dir.toLowerCase()]) {
+            directions.pop();
+            continue;
+        }
+        else {
+            directions.push(dir);
+        }
     }
-    console.log(counts);
+    return directions;
+}
+function dirReduc(arr) {
+    const opposite = {
+        NORTH: 'SOUTH',
+        SOUTH: 'NORTH',
+        EAST: 'WEST',
+        WEST: 'EAST',
+    };
+    return arr.reduce((steps, dir) => {
+        if (steps[steps.length - 1] === opposite[dir]) {
+            steps.pop();
+        }
+        else {
+            steps.push(dir);
+        }
+        return steps;
+    }, []);
 }
 console.log(dirReduc(['NORTH', 'SOUTH', 'SOUTH', 'EAST', 'WEST', 'NORTH', 'WEST']), ['WEST']);
 console.log(dirReduc(['NORTH', 'WEST', 'SOUTH', 'EAST']), ['NORTH', 'WEST', 'SOUTH', 'EAST']);
 console.log(dirReduc(['NORTH', 'SOUTH', 'EAST', 'WEST', 'EAST', 'WEST']), []);
+console.log(dirReduc([]), []);
--- 2020-09-23/directions-reductions.ts
+++ 2020-09-23/directions-reductions.ts
@@ -1,15 +1,54 @@
 function dirReduc(arr) {
-    const counts = {};
-
-    for (let i = 0; i < arr.length; i++) {
-        const direction = arr[i].toLowerCase();
-        counts[direction] = counts[direction] || 0;
-        counts[direction]++;
+    // if initial array is empty return it
+    if (!arr.length) return arr;
+    // lookup object
+    const counterDirections = {
+        north: 'south',
+        south: 'north',
+        east: 'west',
+        west: 'east',
+    };
+    // place to store simplified directions
+    const directions = [];
+    // put first direction into directions array
+    directions.push(arr[0]);
+    // iterate over the directions
+    for (let i = 1; i < arr.length; i++) {
+        const dir = arr[i];
+        // if last item from directions array is a counter direction pop it from array and skip current
+        if (
+            directions.length &&
+            directions[directions.length - 1].toLowerCase() === counterDirections[dir.toLowerCase()]
+        ) {
+            directions.pop();
+            continue;
+        } else {
+            // else push to directions array
+            directions.push(dir);
+        }
     }
 
-    console.log(counts);
+    return directions;
+}
+
+function dirReduc(arr) {
+    const opposite = {
+        NORTH: 'SOUTH',
+        SOUTH: 'NORTH',
+        EAST: 'WEST',
+        WEST: 'EAST',
+    };
+    return arr.reduce((steps, dir) => {
+        if (steps[steps.length - 1] === opposite[dir]) {
+            steps.pop();
+        } else {
+            steps.push(dir);
+        }
+        return steps;
+    }, []);
 }
 
 console.log(dirReduc(['NORTH', 'SOUTH', 'SOUTH', 'EAST', 'WEST', 'NORTH', 'WEST']), ['WEST']);
 console.log(dirReduc(['NORTH', 'WEST', 'SOUTH', 'EAST']), ['NORTH', 'WEST', 'SOUTH', 'EAST']);
 console.log(dirReduc(['NORTH', 'SOUTH', 'EAST', 'WEST', 'EAST', 'WEST']), []);
+console.log(dirReduc([]), []);
