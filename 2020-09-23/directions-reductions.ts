cf8ceea558d4d89ebb203796bc7b1130b34b1171
PERSCENT\jakub.marciniszyn <jakub.marciniszyn@tfs.com>
2020-09-28 10:53:21 +0100

    refac

--- 2020-09-23/directions-reductions.ts
+++ 2020-09-23/directions-reductions.ts
@@ -1,37 +1,4 @@
 function dirReduc(arr) {
-    // if initial array is empty return it
-    if (!arr.length) return arr;
-    // lookup object
-    const counterDirections = {
-        north: 'south',
-        south: 'north',
-        east: 'west',
-        west: 'east',
-    };
-    // place to store simplified directions
-    const directions = [];
-    // put first direction into directions array
-    directions.push(arr[0]);
-    // iterate over the directions
-    for (let i = 1; i < arr.length; i++) {
-        const dir = arr[i];
-        // if last item from directions array is a counter direction pop it from array and skip current
-        if (
-            directions.length &&
-            directions[directions.length - 1].toLowerCase() === counterDirections[dir.toLowerCase()]
-        ) {
-            directions.pop();
-            continue;
-        } else {
-            // else push to directions array
-            directions.push(dir);
-        }
-    }
-
-    return directions;
-}
-
-function dirReduc(arr) {
     const opposite = {
         NORTH: 'SOUTH',
         SOUTH: 'NORTH',
