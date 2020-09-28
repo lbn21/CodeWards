0ec02867e68c64f042ff8ffc009e38c1388b426e
PERSCENT\jakub.marciniszyn <jakub.marciniszyn@tfs.com>
2020-09-28 09:17:42 +0100

    who likes it

---
+++ 2020-09-28/who-likes-it.js
@@ -0,0 +1,16 @@
+function likes(names) {
+    if (!names.length)
+        return 'no one likes this';
+    if (names.length === 1)
+        return `${names[0]} likes this`;
+    if (names.length === 2)
+        return `${names[0]} and ${names[1]} like this`;
+    if (names.length === 3)
+        return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+    return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+}
+console.log(likes([]), 'no one likes this');
+console.log(likes(['Peter']), 'Peter likes this');
+console.log(likes(['Jacob', 'Alex']), 'Jacob and Alex like this');
+console.log(likes(['Max', 'John', 'Mark']), 'Max, John and Mark like this');
+console.log(likes(['Alex', 'Jacob', 'Mark', 'Max']), 'Alex, Jacob and 2 others like this');
