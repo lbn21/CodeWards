6b1111c02ba5059609223c4c4e80096eb73cd195
PERSCENT\jakub.marciniszyn <jakub.marciniszyn@tfs.com>
2020-09-29 10:33:22 +0100

    breadcrumbs init

---
+++ 2020-09-29/breadcrumbs.ts
@@ -0,0 +1,30 @@
+const generateBC = (url, separator) => {
+    //your code here
+    return;
+};
+
+console.log(
+    generateBC('mysite.com/pictures/holidays.html', ' : '),
+    '<a href="/">HOME</a> : <a href="/pictures/">PICTURES</a> : <span class="active">HOLIDAYS</span>',
+);
+
+// console.log(
+//     generateBC('www.microsoft.com/docs/index.htm', ' * '),
+//     '<a href="/">HOME</a> * <span class="active">DOCS</span>',
+// );
+// console.log(
+//     generateBC('www.codewars.com/users/GiacomoSorbi', ' / '),
+//     '<a href="/">HOME</a> / <a href="/users/">USERS</a> / <span class="active">GIACOMOSORBI</span>',
+// );
+// console.log(
+//     generateBC('www.microsoft.com/important/confidential/docs/index.htm#top', ' * '),
+//     '<a href="/">HOME</a> * <a href="/important/">IMPORTANT</a> * <a href="/important/confidential/">CONFIDENTIAL</a> * <span class="active">DOCS</span>',
+// );
+// console.log(
+//     generateBC('mysite.com/very-long-url-to-make-a-silly-yet-meaningful-example/example.asp', ' > '),
+//     '<a href="/">HOME</a> > <a href="/very-long-url-to-make-a-silly-yet-meaningful-example/">VLUMSYME</a> > <span class="active">EXAMPLE</span>',
+// );
+// console.log(
+//     generateBC('www.very-long-site_name-to-make-a-silly-yet-meaningful-example.com/users/giacomo-sorbi', ' + '),
+//     '<a href="/">HOME</a> + <a href="/users/">USERS</a> + <span class="active">GIACOMO SORBI</span>',
+// );
